aaa28595a65683b08b55da2aff91431fc5f1db3b
Blaise <blaisemozes@gmail.com>
2023-11-24 14:52:21 +0200

    feat: add ngrx store-devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,19 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
-import { TodoEffects } from './modules/store/todo/todo.effects';
-import { reducer, TODO_FEATURE_KEY } from './modules/store/todo/todo.reducer';
-
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { StoreModule } from "@ngrx/store";
+import { EffectsModule } from "@ngrx/effects";
+import { TodoEffects } from "./modules/store/todo/todo.effects";
+import { reducer, TODO_FEATURE_KEY } from "./modules/store/todo/todo.reducer";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { environment } from "src/environments/environment";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -24,8 +23,12 @@ import { reducer, TODO_FEATURE_KEY } from './modules/store/todo/todo.reducer';
     EffectsModule.forRoot(),
     StoreModule.forFeature(TODO_FEATURE_KEY, reducer),
     EffectsModule.forFeature([TodoEffects]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
