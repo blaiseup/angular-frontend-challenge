817c7cb3340d97db14571dd315e0bae6f592590d
Blaise <blaisemozes@gmail.com>
2023-11-24 16:56:12 +0200

    fix: make all the current tests pass

--- src/app/modules/todo/components/edit-todo-dialog/edit-todo-dialog.component.spec.ts
+++ src/app/modules/todo/components/edit-todo-dialog/edit-todo-dialog.component.spec.ts
@@ -1,23 +1,33 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { EditTodoDialogComponent } from './edit-todo-dialog.component';
+import { EditTodoDialogComponent } from "./edit-todo-dialog.component";
+import { MatDialogModule, MatDialogRef } from "@angular/material/dialog";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatInputModule } from "@angular/material/input";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
 
-describe('EditTodoDialogComponent', () => {
+describe("EditTodoDialogComponent", () => {
   let component: EditTodoDialogComponent;
   let fixture: ComponentFixture<EditTodoDialogComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ EditTodoDialogComponent ]
-    })
-    .compileComponents();
+      declarations: [EditTodoDialogComponent],
+      imports: [MatDialogModule, MatInputModule, FormsModule, ReactiveFormsModule, NoopAnimationsModule],
+      providers: [
+        {
+          provide: MatDialogRef,
+          useValue: {},
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(EditTodoDialogComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
--- src/app/modules/todo/todo.component.spec.ts
+++ src/app/modules/todo/todo.component.spec.ts
@@ -1,23 +1,29 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { provideMockStore } from "@ngrx/store/testing";
+import { TodoComponent } from "./todo.component";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatTabsModule } from "@angular/material/tabs";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { MatInputModule } from "@angular/material/input";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
-import { TodoComponent } from './todo.component';
-
-describe('TodoComponent', () => {
+describe("TodoComponent", () => {
   let component: TodoComponent;
   let fixture: ComponentFixture<TodoComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TodoComponent ]
-    })
-    .compileComponents();
+      imports: [NoopAnimationsModule, MatDialogModule, MatTabsModule, MatInputModule, FormsModule, ReactiveFormsModule],
+      declarations: [TodoComponent],
+      providers: [provideMockStore()],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(TodoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
