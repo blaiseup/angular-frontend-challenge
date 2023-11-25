1f1eac9532de8176ae712ee7214ed8931b658742
Blaise <blaisemozes@gmail.com>
2023-11-25 10:57:18 +0200

    test: add tests to edit todo dialog

--- src/app/modules/todo/components/edit-todo-dialog/edit-todo-dialog.component.spec.ts
+++ src/app/modules/todo/components/edit-todo-dialog/edit-todo-dialog.component.spec.ts
@@ -1,33 +1,38 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
-
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
 import { EditTodoDialogComponent } from "./edit-todo-dialog.component";
-import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from "@angular/material/dialog";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { MatInputModule } from "@angular/material/input";
-import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { Todo, TodoPriority, TodoStatus } from "src/app/models/todo.model";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 
 describe("EditTodoDialogComponent", () => {
   let component: EditTodoDialogComponent;
   let fixture: ComponentFixture<EditTodoDialogComponent>;
 
+  const mockDialogRef = {
+    close: jasmine.createSpy("close"),
+  };
+
+  const mockTodo: Todo = {
+    id: 1,
+    name: "Test Todo",
+    priority: TodoPriority.Low,
+    status: TodoStatus.InProgress,
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [EditTodoDialogComponent],
-      imports: [MatDialogModule, MatInputModule, FormsModule, ReactiveFormsModule, NoopAnimationsModule],
+      imports: [MatDialogModule, BrowserAnimationsModule],
       providers: [
-        {
-          provide: MatDialogRef,
-          useValue: {},
-        },
-        {
-          provide: MAT_DIALOG_DATA,
-          useValue: {},
-        },
+        { provide: MatDialogRef, useValue: mockDialogRef },
+        { provide: MAT_DIALOG_DATA, useValue: { todo: mockTodo } },
       ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(EditTodoDialogComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -35,5 +40,30 @@ describe("EditTodoDialogComponent", () => {
 
   it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should initialize todo from MAT_DIALOG_DATA", () => {
+    expect(component.todo).toEqual(mockTodo);
+  });
+
+  it("should close dialog with updated todo on saveTodo", () => {
+    const newName = "Updated Todo";
+    const newPriority = TodoPriority.Medium;
+
+    component.saveTodo({
+      name: newName,
+      priority: newPriority,
+    });
+
+    expect(mockDialogRef.close).toHaveBeenCalledWith({
+      ...mockTodo,
+      name: newName,
+      priority: newPriority,
+    });
+  });
+
+  it("should close dialog onCancel", () => {
+    component.onCancel();
+    expect(mockDialogRef.close).toHaveBeenCalled();
   });
 });
